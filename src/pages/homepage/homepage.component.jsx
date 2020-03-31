a94a9c4b53b3b8cddd9a6501514cff2096b391bc
thevictorwhyte <victordavidwhyte@gmail.com>
2020-03-31 22:24:05 +0100

    update routes and shit

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './homepage.styles.scss';
+import Directory from '../../components/directory/directory.component';
+
+const Homepage = () => (
+	<Directory />
+)
+
+export default Homepage;
