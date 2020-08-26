2f321488c0a5b0b215bfd55e7ea09a4ae392868c
thevictorwhyte <victordavidwhyte@gmail.com>
2020-08-27 00:00:32 +0100

    replaced thunks with sagas

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -5,25 +5,24 @@ import { connect } from 'react-redux';
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
 import CollectionPageContainer from '../collection/collection.container';
 
-import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
-import { selectIsCollectionsLoaded } from '../../redux/shop/shop.selectors';
+import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
 class ShopPage extends React.Component {
 
-	componentDidMount(){
-		const { fetchCollectionsStartAsync } = this.props;
-		fetchCollectionsStartAsync();
+	componentDidMount() {
+		const { fetchCollectionsStart } = this.props;
+		fetchCollectionsStart();
 	}
 
 
-	render(){
+	render() {
 		const { match } = this.props;
-		return(
-			<div className='shop-page'> 
-				<Route exact path={`${match.path}`}  
-				component={CollectionsOverviewContainer} /> 
-				<Route path={`${match.path}/:collectionId`} 
-					component={CollectionPageContainer}/>
+		return (
+			<div className='shop-page'>
+				<Route exact path={`${match.path}`}
+					component={CollectionsOverviewContainer} />
+				<Route path={`${match.path}/:collectionId`}
+					component={CollectionPageContainer} />
 			</div>
 		);
 	}
@@ -31,8 +30,8 @@ class ShopPage extends React.Component {
 
 
 const mapDispatchtoProps = dispatch => ({
-	fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
-	
+	fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
+
 });
 
 export default connect(null, mapDispatchtoProps)(ShopPage);
