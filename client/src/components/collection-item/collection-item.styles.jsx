d0bf1d630db140a538fdc484b5213c9ed2bd3eba
thevictorwhyte <victordavidwhyte@gmail.com>
2020-09-04 13:10:12 +0100

    bug fixes

--- client/src/components/collection-item/collection-item.styles.jsx
+++ client/src/components/collection-item/collection-item.styles.jsx
@@ -68,11 +68,9 @@ export const CollectionFooterContainer = styled.div`
 `;
 
 export const NameContainer = styled.span`
-  width: 90%;
   margin-bottom: 15px;
 `;
 
 export const PriceContainer = styled.span`
-  width: 10%;
   text-align: right;
 `;
