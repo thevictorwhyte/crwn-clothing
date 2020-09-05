66b43647192b4f08fa49964a64569e570b6dea4e
thevictorwhyte <victordavidwhyte@gmail.com>
2020-09-05 14:54:51 +0100

    adding performance optimization with react lazy, suspense and memo as well as compression on backend

--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,32 +1,42 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
-import CollectionPageContainer from '../collection/collection.container';
+import Spinner from '../../components/spinner/spinner.component';
 
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
+const CollectionsOverviewContainer = lazy(() =>
+  import('../../components/collections-overview/collections-overview.container')
+);
+const CollectionPageContainer = lazy(() =>
+  import('../collection/collection.container')
+);
+
 const ShopPage = ({ fetchCollectionsStart, match }) => {
+  useEffect(() => {
+    fetchCollectionsStart();
+  }, [fetchCollectionsStart]);
 
-	useEffect(() => {
-		fetchCollectionsStart();
-	}, [fetchCollectionsStart])
+  return (
+    <div className="shop-page">
+      <Suspense fallback={<Spinner />}>
+        <Route
+          exact
+          path={`${match.path}`}
+          component={CollectionsOverviewContainer}
+        />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPageContainer}
+        />
+      </Suspense>
+    </div>
+  );
+};
 
-	return (
-		<div className='shop-page'>
-			<Route exact path={`${match.path}`}
-				component={CollectionsOverviewContainer} />
-			<Route path={`${match.path}/:collectionId`}
-				component={CollectionPageContainer} />
-		</div>
-	);
-}
-
-
-const mapDispatchtoProps = dispatch => ({
-	fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
-
+const mapDispatchtoProps = (dispatch) => ({
+  fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
 });
 
 export default connect(null, mapDispatchtoProps)(ShopPage);
